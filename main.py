0aabd1f63e03829292fd97ad3b530614975d434f
sofia-ces <ceciliasalenga25@gmail.com>
2024-12-14 22:44:26 +0800

    Python Converter

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ unit = input("Kilograms or Pounds? (K or L):")
 if unit == "K":
     weight = weight * 2.205
     unit = "Lbs."
+    print(f"Your weight is: {round(weight, 1)} {unit}")
 elif unit == "L":
     weight = weight / 2.205
     unit = "Kgs."
+    print(f"Your weight is: {round(weight, 1)} {unit}")
 else:
     print(f"{unit} was not valid")
 
-print(f"Your weight is: {weight} {unit}")
